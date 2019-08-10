f496963fd8b79ed36bef217507191228bb4db232
Ozkar09 <oscarbeltran99@hotmail.com>
2019-08-09 22:28:25 -0500

    Se ajusta respuesta 200

--- app/controllers/api/v1/pins_controller.rb
+++ app/controllers/api/v1/pins_controller.rb
@@ -10,20 +10,20 @@ class Api::V1::PinsController < ApplicationController
 
   end
 
-  def create
-
-    if autenticate?
-      pin = Pin.new(pin_params)
-      if pin.save
-        render json: pin, status: 201
-      else
-        render json: { errors: pin.errors }, status: 422
-      end
-    else
-      head 401
-    end
-
-  end
+  # def create
+  #
+  #   if autenticate?
+  #     pin = Pin.new(pin_params)
+  #     if pin.save
+  #       render json: pin, status: 201
+  #     else
+  #       render json: { errors: pin.errors }, status: 422
+  #     end
+  #   else
+  #     head 401
+  #   end
+  #
+  # end
 
   def autenticate?
 
